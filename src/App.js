2cbe61772dd39aa92887f029d1ebdc7dfef1cc2a
Gabriel Moura <gabrielmsdf3@gmail.com>
2021-03-29 18:34:05 -0300

    Criando as primeiras classes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-
+import listaDeNotas from "./components/listaDeNotas";
 function App() {
   return (
-   <h1>Meu app React</h1>
+    <section>
+      <form>
+        <input type="text" placeholder="Título"/>
+        <textarea placeholder="Escreva sua nota..." />
+        <button>Criar Nota</button>
+      </form>
+      <listaDeNotas />
+    </section>
   );
 }
 

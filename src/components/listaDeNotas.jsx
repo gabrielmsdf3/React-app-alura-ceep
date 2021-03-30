7a921b7f115ae58b9b244b09b1a022f1f13b92e9
Gabriel Moura <gabrielmsdf3@gmail.com>
2021-03-30 15:11:33 -0300

    array.of

--- src/components/listaDeNotas.jsx
+++ src/components/listaDeNotas.jsx
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import CardNota from './CardNota';
-
+//o map (que devolve uma lista) vai pegar cada uma das categorias em " "e usar como
+//e vai passar pra uma função e na função é preciso 
+//retornar o que vai ser renderizado
+// o array.of ta criando um array com os 3 elementos criados
 class ListaDeNotas extends Component{
   render(){
     return(
       <ul>
-        <li>
-          <CardNota />
-        </li>
-        <li>
-        <CardNota />
-        </li>
-        <li>
-        <CardNota />
-        </li>
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
+          return (
+            <li>
+              <div>{categoria}</div>
+              <CardNota />
+            </li>
+          );
+        })}
+        
       </ul>
     )
   }

77f6c6ae2de4294e065d5c47e4dcbb12073cbdc3
Gabriel Moura <gabrielmsdf3@gmail.com>
2021-04-08 11:20:23 -0300

    referencias funçoes e bind

--- src/components/ListaDeNotas/listaDeNotas.jsx
+++ src/components/ListaDeNotas/listaDeNotas.jsx
@@ -6,11 +6,28 @@ import './estilo.css';
 //retornar o que vai ser renderizado
 // o array.of ta criando um array com os 3 elementos criados
 class ListaDeNotas extends Component{
-  
+  constructor(){
+    super()
+    this.state = {notas:[]}
+    this._novasNotas = this._novasNotas.bind(this)
+  }
+  componentDidMount(){
+    this.props.notas.inscrever(this._novasNotas)
+  }
+
+  componentWillUnmount(){
+    this.props.notas.desinscrever(this._novasNotas)
+
+  }
+
+  _novasNotas(notas){
+this.setState({...this.state, notas})
+  }
+
   render(){
     return(
       <ul className="lista-notas">
-        {this.props.notas.map((nota, index) => {
+        {this.state.notas.map((nota, index) => {
           return (
 
             <li className="lista-notas_item" key={index}>
